013548a8ded59701d98d571b4bf97443657c1deb
Chiquita <francochiquita@gmail.com>
2023-08-29 19:09:48 -0500

    Arithmetic lab

---
+++ index.js
@@ -0,0 +1,75 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+add()
+
+function subtract(a, b) {
+    return a - b;
+}
+subtract()
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply()
+
+function divide(a, b) {
+    return a / b;
+}
+divide()
+
+function increment(n) {
+    return n + 1;
+}
+let number = 2;
+let result = increment(number);
+console.log(result);
+
+function decrement(n) {
+    return n - 1;
+}
+let numb = 2;
+let answer = decrement(number);
+console.log(result);
+
+function makeInt(n) {
+    return parseInt(n, 1);
+}
+let digit = "10"
+let parsedInt = makeInt(digit);
+console.log(parsedInt);
+
+
+function makeInt(n) {
+    return parseInt(n);
+}
+console.log(parsedInt);
+
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+let num = "10"
+let n = base
+console.log(parsedInt);
+
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+let parseFloat = preserveDecimal(n);
+console.log(parseFloat);
